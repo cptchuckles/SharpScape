1830a4167cef99c8a01dff0688d6b42ab54c4177
Sun <41078760+SunnyRo@users.noreply.github.com>
2022-06-17 13:44:48 -0700

    Update Api/Data/Models/Thread.cs
    Co-authored-by: Jonathan Gollnick <32113038+cptchuckles@users.noreply.github.com>

--- Api/Data/Models/Thread.cs
+++ Api/Data/Models/Thread.cs
@@ -13,7 +13,7 @@ public class Thread
     public string Title { get; set; }
     public string Body {get;set;}
     public string Votes { get; set; }
-    public string Relies { get; set; }
+    public int Replies { get; set; }
     public string Views { get; set; }
     [Editable(false)]
     [DisplayFormat(DataFormatString = "{0:D}")]
