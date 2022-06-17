b5ba8f757b60e514f68e14925fececa3d5bf8a73
Sun <41078760+SunnyRo@users.noreply.github.com>
2022-06-17 13:44:31 -0700

    Update Shared/ThreadDto.cs
    Co-authored-by: Jonathan Gollnick <32113038+cptchuckles@users.noreply.github.com>

--- Shared/ThreadDto.cs
+++ Shared/ThreadDto.cs
@@ -2,9 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SharpScape.Shared.Dto;
 
-public class ThreadDto 
+public class ThreadInfoDto 
 {
     [Required]
     public Guid Id { get; set; }
-
+    public string Title { get; set; }
+    public int Votes { get; set; }
+    public int Replies { get; set; }
+    public int Views { get; set; }
 }
