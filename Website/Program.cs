cbd8f626fa571f3b40033a0093d5525732c6048f
RomanShmi <90277345+RomanShmi@users.noreply.github.com>
2022-06-22 19:54:21 -0400

    Update Website/Program.cs
    Co-authored-by: Jonathan Gollnick <32113038+cptchuckles@users.noreply.github.com>

--- Website/Program.cs
+++ Website/Program.cs
@@ -9,10 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
-
-
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IAuthTokenProvider, AuthTokenProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider, AppAuthStateProvider>();
