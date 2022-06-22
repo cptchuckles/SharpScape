de98188237203d76cdd9e4a40420aa458f4e8263
RomanShmi <90277345+RomanShmi@users.noreply.github.com>
2022-06-22 19:54:50 -0400

    Update Website/Services/ForumCategoryServices.cs
    Co-authored-by: Jonathan Gollnick <32113038+cptchuckles@users.noreply.github.com>

--- Website/Services/ForumCategoryServices.cs
+++ Website/Services/ForumCategoryServices.cs
@@ -21,33 +21,5 @@ namespace SharpScape.Website.Services
             {
                 return await httpClient.GetFromJsonAsync<List<ForumCategoryDto>>("api/ForumCategories");
             }
-       
-
-
     }
 }
-
-
-/*using EmployeeManagement.Models;
-using Microsoft.AspNetCore.Components;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
-
-namespace EmployeeManagement.Web.Services
-{
-    public class EmployeeService : IEmployeeService
-    {
-        private readonly HttpClient httpClient;
-
-        public EmployeeService(HttpClient httpClient)
-        {
-            this.httpClient = httpClient;
-        }
-
-        public async Task<IEnumerable<Employee>> GetEmployees()
-        {
-            return await httpClient.GetJsonAsync<Employee[]>("api/employees");
-        }
-    }
-}*/
