4495044c3334b929521b60a0faa89cab916ee20b
Sun <alex.vo.1993@gmail.com>
2022-06-30 08:31:19 -0700

    fixed forumindex page(icons,remove views stat), remove blank space at bottom of all pages

--- Website/Services/IForumCategoryServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using SharpScape.Shared.Dto;
-
-namespace SharpScape.Website.Services
-{
-    public interface IForumCategoryServices
-    {
-        Task<List<ForumCategoryDto>> GetForumCategories();
-    }
-}
