6acfd7ea1f51cf6ccb4d30d642c55b3d7cd7d198
Rachel Hoffman <tailsnick5@gmail.com>
2022-07-05 13:06:40 -0600

    Token start

---
+++ Api/Services/TokenService.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace SharpScape.Api.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
