17176021c33613f91219744dbcc899bbb1b8d65b
Grendel <jcg@grendel.world>
2022-07-07 17:11:47 -0500

    Add Postgres migrations as of now

---
+++ Api/Data/Migrations/Postgres/20220706002920_Initialize.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SharpScape.Api.Data.Migrations.Postgres
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ForumCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "bytea", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "bytea", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ForumThreads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    Votes = table.Column<int>(type: "integer", nullable: false),
+                    Replies = table.Column<int>(type: "integer", nullable: false),
+                    Views = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumThreads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ForumThreads_ForumCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "ForumCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ForumThreads_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameAvatars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    SpriteName = table.Column<string>(type: "text", nullable: false),
+                    GlobalPositionX = table.Column<float>(type: "real", nullable: false),
+                    GlobalPositionY = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameAvatars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameAvatars_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ForumPosts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ThreadId = table.Column<int>(type: "integer", nullable: false),
+                    AuthorId = table.Column<int>(type: "integer", nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumPosts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ForumPosts_ForumThreads_ThreadId",
+                        column: x => x.ThreadId,
+                        principalTable: "ForumThreads",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ForumPosts_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumPosts_AuthorId",
+                table: "ForumPosts",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumPosts_ThreadId",
+                table: "ForumPosts",
+                column: "ThreadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumThreads_CategoryId",
+                table: "ForumThreads",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumThreads_UserId",
+                table: "ForumThreads",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameAvatars_UserId",
+                table: "GameAvatars",
+                column: "UserId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ForumPosts");
+
+            migrationBuilder.DropTable(
+                name: "GameAvatars");
+
+            migrationBuilder.DropTable(
+                name: "ForumThreads");
+
+            migrationBuilder.DropTable(
+                name: "ForumCategories");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Api/Data/Migrations/Postgres/20220706200634_Seed_Categories.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using SharpScape.Api.Models;
+
+#nullable disable
+
+namespace SharpScape.Api.Data.Migrations.Postgres
+{
+    public partial class Seed_Categories : Migration
+    {
+        private List<ForumCategory> forumCategories = new() {
+            new ForumCategory(){ 
+                Id = 1, 
+                Name = "General Discussion", 
+                Description = "Talk about sports, entertainment, music, movies, your favorite color, talk about enything."
+            },
+            new ForumCategory(){ 
+                Id = 2, 
+                Name = "Introductions", 
+                Description = "New to the community? Please stop by, say hi and tell us a bit about yourself."
+            },
+            new ForumCategory(){ 
+                Id = 3, 
+                Name = "Announcements", 
+                Description = "This forum features announcements from the community staff. If there is a new post in this forum, please check it out."
+            },
+            new ForumCategory(){ 
+                Id = 4, 
+                Name = "Staff Discussion", 
+                Description = "This forum is for private, staff member only discussions, usually pertaining to the community itself."
+            },
+            new ForumCategory(){ 
+                Id = 5, 
+                Name = "Lorem Ipsum is simply dummy text", 
+                Description = "Various versions have evolved over the years, sometimes by accident, sometimes on purpose passage of Lorem Ipsum (injected humour and the like)."
+            },
+            new ForumCategory(){ 
+                Id = 6, 
+                Name = "There are many variations of passages", 
+                Description = "If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the."
+            },
+            new ForumCategory(){ 
+                Id = 7, 
+                Name = "The standard chunk of Lorem Ipsum", 
+                Description = "Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet."
+            },
+            new ForumCategory(){ 
+                Id = 8, 
+                Name = "Lorem Ipsum, you need to be sure there", 
+                Description = "Internet tend to repeat predefined chunks as necessary, making this the."
+            }
+        };
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            foreach (var forumCategory in forumCategories)
+            {
+                migrationBuilder.InsertData("ForumCategories",
+                    new[] { "Id", "Name", "Description" },
+                    new object[] { forumCategory.Id, forumCategory.Name, forumCategory.Description });
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            foreach (var forumCategory in forumCategories)
+            {
+                migrationBuilder.DeleteData("ForumCategories", "Id", forumCategory.Id);
+            }
+        }
+    }
+}
