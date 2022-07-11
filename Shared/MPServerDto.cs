56948e0af399281d889bc0085b1c8531b743cc02
Grendel <jcg@grendel.world>
2022-07-11 14:29:26 -0500

    Add Save endpoint to MPServerController

--- Shared/MPServerDto.cs
+++ Shared/MPServerDto.cs
@@ -1,23 +1,23 @@
 namespace SharpScape.Shared.Dto;
 
-public class MPServerLoginDto
+public class MPServerMessageDto
 {
-    public string Payload { get; set; }
+    public string Payload { get; set; } = string.Empty;
     public int Timestamp { get; set; }
-    public string Signature { get; set; }
+    public string Signature { get; set; } = string.Empty;
 }
 
 public class MPUniqueSecret
 {
     public Guid KeyId { get; set; }
-    public string SecureKey { get; set; }
-    public string Payload { get; set; }
+    public string SecureKey { get; set; } = string.Empty;
+    public string Payload { get; set; } = string.Empty;
 }
 
 public class MPCryptoKey
 {
     public Guid KeyId { get; set; }
-    public string X509Pub { get; set; }
+    public string X509Pub { get; set; } = string.Empty;
     public MPCryptoKey(Guid keyId, string x509pub)
     {
         KeyId = keyId;
@@ -28,7 +28,14 @@ public class MPCryptoKey
 public class GameAvatarInfoDto
 {
     public UserInfoDto UserInfo { get; set; }
-    public string SpriteName { get; set; }
+    public string SpriteName { get; set; } = string.Empty;
+    public float GlobalPositionX { get; set; }
+    public float GlobalPositionY { get; set; }
+}
+
+public class GameSaveDto
+{
+    public int UserId { get; set; }
     public float GlobalPositionX { get; set; }
     public float GlobalPositionY { get; set; }
 }
