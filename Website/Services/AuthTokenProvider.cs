d1b56cdd7bda88975039b9c784e37fa288f5611b
Rachel Hoffman <tailsnick5@gmail.com>
2022-07-12 14:31:36 -0600

    Added refresh token in AuthTokenProvider

--- Website/Services/AuthTokenProvider.cs
+++ Website/Services/AuthTokenProvider.cs
@@ -3,11 +3,13 @@ namespace SharpScape.Website.Services;
 public interface IAuthTokenProvider
 {
     public string? Token { get; set; }
+    public string? RefreshToken { get; set; }
 }
 
 public class AuthTokenProvider : IAuthTokenProvider
 {
     public string? Token { get; set; }
+    public string? RefreshToken { get; set; }
     public AuthTokenProvider()
     {
     }
