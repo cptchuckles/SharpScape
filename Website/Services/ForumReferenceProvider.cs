7af0de430ca1e062e0c6e82a69a99f57175980a9
Roman Shmigelskyi <romyli4ka@gmail.com>
2022-07-13 15:31:29 -0400

    loged id on page no shared buffer

--- Website/Services/ForumReferenceProvider.cs
+++ Website/Services/ForumReferenceProvider.cs
@@ -1,10 +1,10 @@
 namespace SharpScape.Website.Services
 {
-    public class ForumReferenceProvider
-    {
-        public int CategoryId { get; set; } = 0;
-        public int ThreadId { get; set; } = 0;
-        public int UserId { get; set; } = 0;
-        public string UserName { get; set; } = null;
-    }
+   // public class ForumReferenceProvider
+    //{
+     //   public int CategoryId { get; set; } = 0;
+      //  public int ThreadId { get; set; } = 0;
+       // public int UserId { get; set; } = 0;
+     //   public string UserName { get; set; } = null;
+    
 }
