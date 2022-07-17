5d49f6d66c3ff39ee556f78b77bf3a84cf888f21
Sun <alex.vo.1993@gmail.com>
2022-07-16 21:57:17 -0700

    added like-a-thread

---
+++ Api/Data/Models/ThreadLike.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SharpScape.Api.Models
+{
+    public class ThreadLike 
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public  User? User { get; set; }
+
+        public int ThreadId { get; set; }
+        [ForeignKey("ThreadId")]
+        public ForumThread ForumThread { get; set; }
+
+    }
+}
