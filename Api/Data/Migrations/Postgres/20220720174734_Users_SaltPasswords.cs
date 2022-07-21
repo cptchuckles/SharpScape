491be12c31746d792d593b21ac03070c11010435
Grendel <jcg@grendel.world>
2022-07-21 13:39:42 -0500

    Migrate Postgres db context for salted passwords

---
+++ Api/Data/Migrations/Postgres/20220720174734_Users_SaltPasswords.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharpScape.Api.Data.Migrations.Postgres
+{
+    public partial class Users_SaltPasswords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHmacKey",
+                table: "Users",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHmacKey",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "bytea",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
